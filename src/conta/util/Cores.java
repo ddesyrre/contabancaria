b192363d8b093882e928534ae425241bc5b42977
ddesyrre <ddesyrre@icloud.com>
2024-04-29 14:51:05 -0300

    contabancaria

---
+++ src/conta/util/Cores.java
@@ -0,0 +1,16 @@
+package conta.util;
+
+import java.util.Scanner;
+
+public class Cores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao;
+
+	}
+
+}
